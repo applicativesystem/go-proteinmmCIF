3458b464db47af49fbd5575c8332be9e8ecdeca4
Gaurav Sablok <gauravcodepro@gmail.com>
2024-11-21 22:56:52 +0100

    applicativesystem
    started to write the bellford algorithm for the continous search of the linear graph path using the protein spectra.

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+/*
+
+Author Gaurav Sablok
+Universitat Potsdam
+Date 2024-11-22
+
+A golang implementation of the linear search space using the BellFord algorithm 
+on the protein spectra and continous CIF reader. 
+
+*/
